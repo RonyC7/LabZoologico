1d5f49b52896b4c4721a8a6ebfdb1122f20e309d
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-05-20 08:41:13 -0600

    Listo Funcionamiento, falta el de editar y eliminar

---
+++ Clases/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab15Zoologico.Clases 
+{
+    public class Animal
+    {
+        public string NombrePropio { get; set; }
+        public string Especie { get; set; }
+        public string TipoAlimentacion { get; set; }
+        public int TiempoMaximoVida { get; set; }
+    }
+
+    public class Ave : Animal
+    {
+        public bool Vuela { get; set; }
+        public int PeriodoIncubacion { get; set; }
+    }
+
+    public class Serpiente : Animal
+    {
+        public bool Venenosa { get; set; }
+        public float LongitudMaxima { get; set; }
+    }
+}
